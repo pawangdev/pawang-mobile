375438498256a58256a30f990db8889129f825b3
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-04-10 11:06:20 +0700

    refactor folder structur & add opensans fonts & add theme constants

---
+++ lib/config/theme_constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const Color kPurple = Color(0xFF6C72CB);
+const Color kBlack = Color(0xFF001833);
+const Color kWarning = Color(0xFFFAD202);
+const Color kSuccess = Color(0xFF64BC26);
+
+// Padding
+const double kDefaultPadding = 16.0;
+const double kDefaultPadding2x = 32.0;
+
+// Border Radius
+const double kDefaultBorderRadius = 12.0;
+
+// Fonts
+TextStyle kOpenSans = const TextStyle(fontFamily: 'OpenSans');
+
+// Font Weight
+FontWeight light = FontWeight.w300;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
