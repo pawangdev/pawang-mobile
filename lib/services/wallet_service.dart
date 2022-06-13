01969da84709a534ffa3781f8b76252c20e1ade8
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-06-13 19:39:22 +0700

    ✨ feat: add service delete & update wallets

--- lib/services/wallet_service.dart
+++ lib/services/wallet_service.dart
@@ -18,7 +18,7 @@ class WalletService {
     if (response.statusCode == 200) {
       return WalletsModel.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception(response.body);
+      throw Exception(false);
     }
   }
 
@@ -39,11 +39,50 @@ class WalletService {
         });
 
     if (response.statusCode == 201) {
-      print(response.statusCode);
       return true;
     } else {
-      print(response.statusCode);
-      return false;
+      throw Exception(false);
+    }
+  }
+
+  Future<bool> updateWallet(Map<String, dynamic> data, int idWallet) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString("token");
+
+    var dataWallet = <String, dynamic>{
+      'name': data['name'],
+      'balance': data['balance'],
+    };
+
+    var response = await http.put(
+        Uri.parse(baseURLAPI + "wallets/$idWallet/update"),
+        body: jsonEncode(dataWallet),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': "Bearer $token",
+        });
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      throw Exception(false);
+    }
+  }
+
+  Future<bool> deleteWallet(int idWallet) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString("token");
+
+    var response = await http
+        .delete(Uri.parse(baseURLAPI + "wallets/$idWallet/delete"), headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': "Bearer $token",
+    });
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      throw Exception(false);
     }
   }
 }
