a5eed1b3001ab6ff5f32c41a991a573349a9193f
Muadz Fathulloh <muadzfathulloh@gmail.com>
2022-09-14 21:21:36 +0700

    feature: add onboarding page

---
+++ lib/views/onboarding/onboarding_controllers.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pawang_mobile/routes/app_pages.dart';
+
+class OnboardingController extends GetxController {
+  var selectedPageIndex = 0.obs;
+  bool get isLastPage => selectedPageIndex.value == onboardingPages.length - 1;
+  var pageController = PageController();
+
+  forwardAction() {
+    if (isLastPage) {
+      // Get.toNamed(RoutesName.splash);
+    } else
+      pageController.nextPage(duration: 300.milliseconds, curve: Curves.ease);
+  }
+
+  List<OnboardingInfo> onboardingPages = [
+    OnboardingInfo('assets/images/girl_boy_landing.png', 'Onboard 1',
+        'Bintang Bintang Bintang BintangBintang BintangBintang .'),
+    OnboardingInfo('assets/images/girl_boy_landing.png', 'Onboard 2',
+        'Bintang Bintang Bintang BintangBintang BintangBintang .'),
+    OnboardingInfo('assets/images/girl_boy_landing.png', 'Onboard 3',
+        'Bintang Bintang Bintang BintangBintang BintangBintang ')
+  ];
+}
+
+class OnboardingInfo {
+  final imageAsset;
+  final title;
+  final description;
+
+  OnboardingInfo(this.imageAsset, this.title, this.description);
+}
