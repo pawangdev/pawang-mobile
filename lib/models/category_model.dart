16a746c5d2e34bfa0f4b58cc98172c69b76fa289
Muadz Fathulloh <muadzfathulloh@gmail.com>
2022-10-17 09:56:16 +0700

    refactor: re-design category

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,3 +1,77 @@
+// // To parse this JSON data, do
+// //
+// //     final categoryModel = categoryModelFromJson(jsonString);
+
+// import 'package:meta/meta.dart';
+// import 'dart:convert';
+
+// CategoryModel categoryModelFromJson(String str) =>
+//     CategoryModel.fromJson(json.decode(str));
+
+// String categoryModelToJson(CategoryModel data) => json.encode(data.toJson());
+
+// class CategoryModel {
+//   CategoryModel({
+//    required this.message,
+//    required this.data,
+//   });
+
+//   String message;
+//   List<CategoryDataModel> data;
+
+//   factory CategoryModel.fromJson(Map<String, dynamic> json) => CategoryModel(
+//         message: json["message"],
+//         data: List<CategoryDataModel>.from(
+//             json["data"].map((x) => CategoryDataModel.fromJson(x))),
+//       );
+
+//   Map<String, dynamic> toJson() => {
+//         "message": message,
+//         "data": List<dynamic>.from(data.map((x) => x.toJson())),
+//       };
+// }
+
+// class CategoryDataModel {
+//   CategoryDataModel({
+//    required this.id,
+//    required this.name,
+//    required this.icon,
+//    required this.type,
+//    required this.createdAt,
+//    required this.updatedAt,
+//    required this.subcategories,
+//   });
+
+//   int id;
+//   String name;
+//   String icon;
+//   String type;
+//   DateTime createdAt;
+//   DateTime updatedAt;
+//   List<dynamic> subcategories;
+
+//   factory CategoryDataModel.fromJson(Map<String, dynamic> json) =>
+//       CategoryDataModel(
+//         id: json["id"],
+//         name: json["name"],
+//         icon: json["icon"],
+//         type: json["type"],
+//         createdAt: DateTime.parse(json["created_at"]),
+//         updatedAt: DateTime.parse(json["updated_at"]),
+//         subcategories: List<dynamic>.from(json["subcategories"].map((x) => x)),
+//       );
+
+//   Map<String, dynamic> toJson() => {
+//         "id": id,
+//         "name": name,
+//         "icon": icon,
+//         "type": type,
+//         "created_at": createdAt.toIso8601String(),
+//         "updated_at": updatedAt.toIso8601String(),
+//         "subcategories": List<dynamic>.from(subcategories.map((x) => x)),
+//       };
+// }
+
 // To parse this JSON data, do
 //
 //     final categoryModel = categoryModelFromJson(jsonString);
@@ -48,7 +122,7 @@ class CategoryDataModel {
   String type;
   DateTime createdAt;
   DateTime updatedAt;
-  List<dynamic> subcategories;
+  List<SubcategoryDataModel> subcategories;
 
   factory CategoryDataModel.fromJson(Map<String, dynamic> json) =>
       CategoryDataModel(
@@ -58,7 +132,8 @@ class CategoryDataModel {
         type: json["type"],
         createdAt: DateTime.parse(json["created_at"]),
         updatedAt: DateTime.parse(json["updated_at"]),
-        subcategories: List<dynamic>.from(json["subcategories"].map((x) => x)),
+        subcategories: List<SubcategoryDataModel>.from(
+            json["subcategories"].map((x) => SubcategoryDataModel.fromJson(x))),
       );
 
   Map<String, dynamic> toJson() => {
@@ -68,6 +143,40 @@ class CategoryDataModel {
         "type": type,
         "created_at": createdAt.toIso8601String(),
         "updated_at": updatedAt.toIso8601String(),
-        "subcategories": List<dynamic>.from(subcategories.map((x) => x)),
+        "subcategories":
+            List<dynamic>.from(subcategories.map((x) => x.toJson())),
+      };
+}
+
+class SubcategoryDataModel {
+  SubcategoryDataModel({
+    required this.id,
+    required this.name,
+    required this.type,
+    required this.userId,
+    required this.categoryId,
+  });
+
+  int id;
+  String name;
+  String type;
+  int userId;
+  int categoryId;
+
+  factory SubcategoryDataModel.fromJson(Map<String, dynamic> json) =>
+      SubcategoryDataModel(
+        id: json["id"],
+        name: json["name"],
+        type: json["type"],
+        userId: json["user_id"],
+        categoryId: json["category_id"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "type": type,
+        "user_id": userId,
+        "category_id": categoryId,
       };
 }
--- lib/modules/category/views/category_view.dart
+++ lib/modules/category/views/category_view.dart
@@ -1,3 +1,4 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
@@ -11,6 +12,7 @@ import 'package:get/get.dart';
 
 class CategoryView extends StatelessWidget {
   final CategoryController controller = Get.find();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,65 +78,252 @@ class CategoryView extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
               child: Obx(
-                () => GridView.builder(
-                  clipBehavior: Clip.none,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      childAspectRatio: 2,
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 1,
-                      mainAxisSpacing: 1),
-                  itemCount: controller.categories.length,
-                  itemBuilder: (context, index) {
-                    var category = controller.categories[index];
-                    return Container(
-                      margin:
-                          const EdgeInsets.only(left: 5, right: 5, bottom: 16),
-                      height: 6.4.h,
-                      decoration: BoxDecoration(
-                          color: defaultWhite,
-                          borderRadius: BorderRadius.circular(12),
-                          boxShadow: [
-                            BoxShadow(
-                              color: defaultBlack.withOpacity(0.07),
-                              spreadRadius: 2,
-                              blurRadius: 10,
-                            )
-                          ]),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: SizedBox(
-                          height: 6.4.h,
-                          child: Row(
+                () => ListView.builder(
+                    itemCount: controller.categories.length,
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      var category = controller.categories[index];
+                      return ExpandablePanel(
+                        header: Row(
+                          children: [
+                            // Text(category.icon),
+                            Icon(
+                              TablerIcons.shopping_cart,
+                              color: defaultPrimary,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              category.name,
+                              style: kOpenSans.copyWith(
+                                  fontWeight: semiBold, fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        collapsed: const SizedBox(height: 20),
+                        expanded: Container(
+                          margin: const EdgeInsets.all(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 20.0, right: 18),
-                                child: Container(
-                                  width: 25,
-                                  height: 25,
-                                  // child: SvgPicture.network(
-                                  //     baseHOSTAPI + category.icon,
-                                  //     fit: BoxFit.cover),
-                                  child: Icon(TablerIcons.shopping_cart),
-                                ),
+                              Text(
+                                'Sub kategori',
+                                style: kOpenSans.copyWith(
+                                    fontWeight: light, fontSize: 12),
                               ),
-                              Expanded(
-                                child: Text(
-                                  category.name,
-                                  style: kOpenSans.copyWith(
-                                    fontSize: 12,
-                                    fontWeight: medium,
-                                  ),
-                                ),
+                              SizedBox(
+                                height: 10,
                               ),
+                              // controller.subcategories.map((e) {
+                              //   return Container(
+                              //     child: Text(""),
+                              //   );
+                              // }).toList(),\
+                              Text(
+                                'Belom ada sub kategori',
+                                style: kOpenSans.copyWith(fontWeight: medium),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      Get.defaultDialog(
+                                        title: 'Tambah Sub Kategori',
+                                        titleStyle: kOpenSans.copyWith(
+                                            fontSize: 14, fontWeight: bold),
+                                        textConfirm: 'Save',
+                                        confirmTextColor: defaultWhite,
+                                        buttonColor: defaultPrimary,
+                                        // cancel: ,
+                                        content: Column(
+                                          children: [
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20),
+                                              child: Row(
+                                                children: [
+                                                  Icon(
+                                                    TablerIcons.shopping_cart,
+                                                    color: defaultPrimary,
+                                                    size: 14,
+                                                  ),
+                                                  SizedBox(
+                                                    width: 5,
+                                                  ),
+                                                  Text(
+                                                    category.name,
+                                                    style: kOpenSans.copyWith(
+                                                        fontWeight: semiBold,
+                                                        fontSize: 12),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                            SizedBox(height: 20),
+                                            TextFormField(
+                                              decoration: InputDecoration(
+                                                contentPadding:
+                                                    EdgeInsets.symmetric(
+                                                        vertical: 0,
+                                                        horizontal: 10),
+                                                hintText: 'Isi Sub Kategori',
+                                                hintStyle: kOpenSans.copyWith(
+                                                    fontSize: 12),
+                                                fillColor: defaultPrimary,
+                                                // filled: true,
+                                                border: OutlineInputBorder(
+                                                  gapPadding: 2,
+                                                  borderRadius:
+                                                      BorderRadius.circular(30),
+                                                  // borderSide:
+                                                  //     BorderSide.none
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      );
+                                    },
+                                    child: Container(
+                                      height: 25,
+                                      width: 25,
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: defaultPrimary,
+                                      ),
+                                      child: const Icon(
+                                        Icons.add,
+                                        color: defaultWhite,
+                                        size: 16,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )
                             ],
                           ),
                         ),
-                      ),
-                    );
-                  },
-                ),
+                        builder: (_, collapsed, expanded) => Expandable(
+                          collapsed: collapsed,
+                          expanded: expanded,
+                        ),
+                        theme: const ExpandableThemeData(
+                          animationDuration: Duration(milliseconds: 300),
+                        ),
+                      );
+                    }),
               ),
+              // GetBuilder<CategoryController>(
+              //   init: CategoryController(),
+              //   builder: (controller) {
+              //     return ListView(
+              //       children: [
+              //         ExpansionPanelList(
+              //           // expandedHeaderPadding: EdgeInsets.all(5),
+              //           expansionCallback: (int index, bool isExpanded) {
+              //             // setState(() {
+              //             //   _data[index].isExpanded = !isExpanded;
+              //             // });
+              //             controller.categories[index];
+              //             // controller.categories[index].isExpanded = !isExpanded;
+              //           },
+              //           children:
+              //               controller.categories.map<ExpansionPanel>((data) {
+              //             return ExpansionPanel(
+              //               canTapOnHeader: true,
+              //               headerBuilder:
+              //                   (BuildContext context, bool isExpanded) {
+              //                 return ListTile(
+              //                   title: Row(
+              //                     children: [
+              //                       Icon(
+              //                         Icons.car_crash,
+              //                         color: defaultPrimary,
+              //                       ),
+              //                       SizedBox(width: 10),
+              //                       Text(data.name),
+              //                     ],
+              //                   ),
+              //                 );
+              //               },
+              //               // isExpanded: data.isExpanded,
+              //               body: ListTile(
+              //                   title: Text(data.icon),
+              //                   subtitle: const Text(
+              //                       'To delete this panel, tap the trash can icon'),
+              //                   trailing: const Icon(Icons.delete),
+              //                   onTap: () {}),
+              //             );
+              //           }).toList(),
+              //         ),
+              //       ],
+              //     );
+              //   },
+              // ),
+              // child: Obx(
+              //   () => GridView.builder(
+              //     clipBehavior: Clip.none,
+              //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              //         childAspectRatio: 2,
+              //         crossAxisCount: 2,
+              //         crossAxisSpacing: 1,
+              //         mainAxisSpacing: 1),
+              //     itemCount: controller.categories.length,
+              //     itemBuilder: (context, index) {
+              //       var category = controller.categories[index];
+              //       return Container(
+              //         margin:
+              //             const EdgeInsets.only(left: 5, right: 5, bottom: 16),
+              //         height: 6.4.h,
+              //         decoration: BoxDecoration(
+              //             color: defaultWhite,
+              //             borderRadius: BorderRadius.circular(12),
+              //             boxShadow: [
+              //               BoxShadow(
+              //                 color: defaultBlack.withOpacity(0.07),
+              //                 spreadRadius: 2,
+              //                 blurRadius: 10,
+              //               )
+              //             ]),
+              //         child: Padding(
+              //           padding: const EdgeInsets.all(8.0),
+              //           child: SizedBox(
+              //             height: 6.4.h,
+              //             child: Row(
+              //               children: [
+              //                 Padding(
+              //                   padding: const EdgeInsets.only(
+              //                       left: 20.0, right: 18),
+              //                   child: Container(
+              //                     width: 25,
+              //                     height: 25,
+              //                     // child: SvgPicture.network(
+              //                     //     baseHOSTAPI + category.icon,
+              //                     //     fit: BoxFit.cover),
+              //                     child: Icon(TablerIcons.shopping_cart),
+              //                   ),
+              //                 ),
+              //                 Expanded(
+              //                   child: Text(
+              //                     category.name,
+              //                     style: kOpenSans.copyWith(
+              //                       fontSize: 12,
+              //                       fontWeight: medium,
+              //                     ),
+              //                   ),
+              //                 ),
+              //               ],
+              //             ),
+              //           ),
+              //         ),
+              //       );
+              //     },
+              //   ),
+              // ),
             ),
           ),
         ],
