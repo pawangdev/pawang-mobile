eef6f5873115fd97132286241b6b2bf693ca8b54
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-12-05 19:43:36 +0700

    feat: add in app update

--- lib/modules/splash/controllers/splash_controller.dart
+++ lib/modules/splash/controllers/splash_controller.dart
@@ -1,14 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:in_app_update/in_app_update.dart';
 import 'package:pawang_mobile/routes/app_pages.dart';
 import 'package:pawang_mobile/utils/storage.dart';
 
 class SplashController extends GetxController {
   final version = '5.1.0';
+  AppUpdateInfo? updateInfo;
+  bool flexibleUpdateAvailable = false;
 
   @override
   void onInit() {
+    InAppUpdate.checkForUpdate().then((info) {
+      updateInfo = info;
+    }).catchError((e) {
+      Get.snackbar(
+        'Gagal Mengupdate Aplikasi !',
+        e.toString(),
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+        margin: const EdgeInsets.all(20),
+        icon: const Icon(
+          Icons.cancel,
+          color: Colors.white,
+        ),
+      );
+    });
+
+    checkUpdate();
+
     checkToken();
     super.onInit();
+  }
+
+  Future<void> checkUpdate() async {
+    InAppUpdate.checkForUpdate().then((updateInfo) {
+      if (updateInfo.updateAvailability == UpdateAvailability.updateAvailable) {
+        InAppUpdate.performImmediateUpdate().then((appUpdateResult) {
+          if (appUpdateResult == AppUpdateResult.userDeniedUpdate) {
+            SystemNavigator.pop();
+          }
+        });
+      }
+    });
   }
 
   Future<void> checkToken() async {
